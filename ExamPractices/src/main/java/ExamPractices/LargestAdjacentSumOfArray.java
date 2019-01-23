a857dc96ad4b9cd46149da52719216d4dec93020
rolandovillca <rolandovillca@gmail.com>
2019-01-23 13:27:12 -0500

    Adding enw example larget adjacent sum of array

---
+++ ExamPractices/src/main/java/ExamPractices/LargestAdjacentSumOfArray.java
@@ -0,0 +1,12 @@
+/*
+A function named largestAdjacentSum that iterates through an array 
+computing the sum of adjacent elements and returning the largest such sum.
+
+Examples:
+
+{1, 2, 3, 4} returns 7 because 3+4 is larger than either 1+2 or 2+3
+{18, -12, 9, -10} returns 6 because 18-12 is larger than -12+9 or 9-10
+{1,1,1,1,1,1,1,1,1} returns 2 because all adjacent pairs sum to 2
+{1,1,1,1,1,2,1,1,1} returns 3 because 1+2 or 2+1 is the max sum of adjacent pairs
+*/
+
---
+++ ExamPractices/src/test/java/ExamPractices/LargetAdjacentSumOfArrayTest.java
@@ -0,0 +1,18 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import org.junit.jupiter.api.Test
+
+package ExamPractices;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class LargetAdjacentSumOfArrayTest {
+
+	@Test
+	public void multiplicationOfZeroIntegersShouldReturnZero() {
+		assertEquals(0, 0);
+		assertEquals(true, true);
+		assertEquals(100, 100);
+	}
+
+}
