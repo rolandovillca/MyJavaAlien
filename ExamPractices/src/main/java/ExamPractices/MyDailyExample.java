eeb66bad78f5ac53b9c01593eef52a4dffca8775
rolandovillca <rolandovillca@gmail.com>
2019-02-15 13:09:59 -0500

    refactoring temporal test coding

--- ExamPractices/src/main/java/ExamPractices/MyDailyExample.java
+++ ExamPractices/src/main/java/ExamPractices/MyDailyExample.java
@@ -14,22 +14,15 @@ class MyDailyExample {
         System.out.println(is121Array(new int[]{2, 2, 2}));
 	}
 
-	static int is121Array(int[] a) {
+	static is121Array(int[] a) {
 		if (a.length < 3) return 0;
 		if (a[0] != 1 || a[a.length-1] != 1) return 0;
+
 		int i, j;
 
-		for (i=0, j=a.length; i<a.length && i<j; i++, j--) {
-			if (a[i] != 1 && a[j] !=1) {
-				break;
-			}
+		for (i=0; i<a.length-1 && i<=j; i++, j--) {
+			
 		}
-
-		for (int idx=i; idx<j; idx++) {
-			if (a[idx] != 2) {
-				return 0;
-			}
-		}
-		return 1;
+		return 0
 	}
-}
+} 
