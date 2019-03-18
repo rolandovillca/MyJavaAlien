a7c3d0083cbf663274a105094285923cabec7601
rolandovillca <rolandovillca@gmail.com>
2019-03-18 11:39:21 -0400

    update description of problem

--- ExamPractices/src/main/java/ExamPractices/AdjacentSum.java
+++ ExamPractices/src/main/java/ExamPractices/AdjacentSum.java
@@ -1,3 +1,15 @@
+/*
+A function named largestAdjacentSum that iterates through an array 
+computing the sum of adjacent elements and returning the largest such sum.
+
+Examples:
+if a is               return
+{1, 2, 3, 4}          -> 7 because 3+4 is larger than either 1+2 or 2+3
+{18, -12, 9, -10}     -> 6 because 18-12 is larger than -12+9 or 9-10
+{1,1,1,1,1,1,1,1,1}   -> 2 because all adjacent pairs sum to 2
+{1,1,1,1,1,2,1,1,1}   -> 3 because 1+2 or 2+1 is the max sum of adjacent pairs
+*/
+
 package ExamPractices;
 
 public class AdjacentSum {
