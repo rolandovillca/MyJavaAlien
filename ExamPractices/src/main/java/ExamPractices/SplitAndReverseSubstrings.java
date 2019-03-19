e443779f451a03d5077323287b615f8848edfae0
rolandovillca <rolandovillca@gmail.com>
2019-03-19 07:55:21 -0400

    Adding practices with string

---
+++ ExamPractices/src/main/java/ExamPractices/SplitAndReverseSubstrings.java
@@ -0,0 +1,30 @@
+package ExamPractices;
+
+class SplitAndReverseSubstrings {
+
+	public static void main(String[] args) {
+		System.out.println(reverse("My name is Rolando"));
+		System.out.println(reverse("My  name    is    Rolando"));
+		System.out.println(reverse("MyString"));
+	}
+
+	static String reverse(String str) {
+		String tmp = "";
+		String tmpRev = "";
+		String rev = "";
+
+		for(int i=0; i<str.length(); i++) {
+			if (str.charAt(i) == ' ' || i == str.length()-1) {
+				for (int j=tmp.length()-1; j>=0; j--) {
+					tmpRev = tmpRev + tmp.charAt(j);
+				}
+				rev = rev + tmpRev + " ";
+				tmpRev = "";
+				tmp = "";
+			} else {
+				tmp = tmp + str.charAt(i);
+			}
+		}
+		return rev;
+	}
+}
