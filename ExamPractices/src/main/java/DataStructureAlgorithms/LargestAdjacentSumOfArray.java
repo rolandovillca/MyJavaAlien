72d86e32438d55051b19ca373693da6c5042aac2
rolandovillca <rolandovillca@gmail.com>
2019-03-24 13:28:46 -0400

    Adding unittests and documentation of excercise

--- ExamPractices/src/main/java/DataStructureAlgorithms/LargestAdjacentSumOfArray.java
+++ ExamPractices/src/main/java/DataStructureAlgorithms/LargestAdjacentSumOfArray.java
@@ -1,19 +1,34 @@
-/*
-A function named largestAdjacentSum that iterates through an array 
-computing the sum of adjacent elements and returning the largest such sum.
-
-Examples:
-
-{1, 2, 3, 4} returns 7 because 3+4 is larger than either 1+2 or 2+3
-{18, -12, 9, -10} returns 6 because 18-12 is larger than -12+9 or 9-10
-{1,1,1,1,1,1,1,1,1} returns 2 because all adjacent pairs sum to 2
-{1,1,1,1,1,2,1,1,1} returns 3 because 1+2 or 2+1 is the max sum of adjacent pairs
-*/
+/**
+ * A function named largestAdjacentSum that iterates through an array 
+ * computing the sum of adjacent elements and returning the largest such sum.
+ *
+ * Examples:
+ *
+ * {1, 2, 3, 4} returns 7 because 3+4 is larger than either 1+2 or 2+3
+ * {18, -12, 9, -10} returns 6 because 18-12 is larger than -12+9 or 9-10
+ * {1,1,1,1,1,1,1,1,1} returns 2 because all adjacent pairs sum to 2
+ * {1,1,1,1,1,2,1,1,1} returns 3 because 1+2 or 2+1 is the max sum of adjacent pairs
+ */
 
 package DataStructureAlgorithms;
 
 class LargetAdjacentSumOfArray {
 	public static void main(String[] args) {
-		System.out.println("Initial method");
+		System.out.println("7 = " + sum(new int[]{1, 2, 3, 4}));
+		System.out.println("6 = " + sum(new int[]{18, -12, 9, -10}));
+		System.out.println("2 = " + sum(new int[]{1,1,1,1,1,1,1,1,1}));
+		System.out.println("3 = " + sum(new int[]{1,1,1,1,1,2,1,1,1}));
+	}
+
+	static int sum(int[] arr) {
+		if (arr.length < 2) return 0;
+		int adjacentSum = arr[0] + arr[1];
+		int tmp;
+
+		for (int i=1; i<arr.length-1; i++) {
+			tmp = arr[i] + arr[i+1];
+			if (tmp > adjacentSum) adjacentSum =+ tmp;
+		}
+		return adjacentSum;
 	}
 }
--- ExamPractices/src/test/java/DataStructureAlgorithms/LargetAdjacentSumOfArrayTest.java
+++ ExamPractices/src/test/java/DataStructureAlgorithms/LargetAdjacentSumOfArrayTest.java
@@ -1,16 +1,56 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-// import org.junit.jupiter.api.Test
+/**
+ * A function named largestAdjacentSum that iterates through an array 
+ * computing the sum of adjacent elements and returning the largest such sum.
+ *
+ * Examples:
+ *
+ * {1, 2, 3, 4} returns 7 because 3+4 is larger than either 1+2 or 2+3
+ * {18, -12, 9, -10} returns 6 because 18-12 is larger than -12+9 or 9-10
+ * {1,1,1,1,1,1,1,1,1} returns 2 because all adjacent pairs sum to 2
+ * {1,1,1,1,1,2,1,1,1} returns 3 because 1+2 or 2+1 is the max sum of adjacent pairs
+ */
 
 package DataStructureAlgorithms;
 
 import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class LargetAdjacentSumOfArrayTest {
+
+	@Before
+	public void before() throws Exception {
+		System.out.println("Setting up - LargetAdjacentSumOfArrayTest");
+	}
+
+	@After
+	public void after() throws Exception {
+		System.out.println("Setting up - LargetAdjacentSumOfArrayTest");
+	}
+
 	@Test
-	public void multiplicationOfZeroIntegersShouldReturnZero() {
-		assertEquals(0, 0);
-		assertEquals(true, true);
-		assertEquals(100, 100);
+	public void sum1() {
+		int[] arr = new int[]{1, 2, 3, 4};
+		assertEquals(7, LargetAdjacentSumOfArray.sum(arr));
+	}
+
+	@Test
+	public void sum2() {
+		int[] arr = new int[]{18, -12, 9, -10};
+		assertEquals(6, LargetAdjacentSumOfArray.sum(arr));
+	}
+
+	@Test
+	public void sum3() {
+		int[] arr = new int[]{1,1,1,1,1,1,1,1,1};
+		assertEquals(2, LargetAdjacentSumOfArray.sum(arr));
+	}
+
+	@Test
+	public void sum4() {
+		int[] arr = new int[]{1,1,1,1,1,2,1,1,1};
+		assertEquals(3, LargetAdjacentSumOfArray.sum(arr));
 	}
 }
