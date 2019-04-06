773003bcdcf665eff9597566711bed7eddec8859
rolandovillca <rolandovillca@gmail.com>
2019-04-06 17:25:42 -0400

    factoring generics and results

--- ExamPractices/src/main/java/GenericPractices/LowerBoundedWilcard.java
+++ ExamPractices/src/main/java/GenericPractices/LowerBoundedWilcard.java
@@ -13,6 +13,14 @@
  * Example
  * Following example illustrates how super is used to specify 
  * an lower bound wildcard.
+ * 
+ * Upper Bounded Wildcards:
+ * The upper bounded wildcard, <? extends T>, where T is any type, 
+ * matches T and any subtype of T.
+ * The process method can access the list elements as type Foo:
+ *
+ * Lower Bounded Wildcards:
+ 
  */
 
 package GenericPractices;
@@ -22,22 +30,32 @@ import java.util.List;
 
 class LowerBoundedWilcard {
     public static void main(String[] args) {
+        //Common generics:
         List<Animal> animals = new ArrayList<Animal>();
         List<BolivianCat> boliviaCats = new ArrayList<BolivianCat>();
         List<Dog> dogs = new ArrayList<Dog>();
 
-        List<Cat> cats = new ArrayList<>();
+        List<Cat> cats = new ArrayList<>(); //New notation above java 8.
         cats.add(new Cat());
         cats.add(new Cat());
         cats.add(new Cat());
-        List<? extends Cat> covariantCats = cats;
-        Animal animal = covariantCats.get(0);
-        Cat cat = covariantCats.get(1);
+        cats.add(new RedCat());
+        cats.add(new CbbaCat());
+        cats.add(new CbbaCat());
+        List<? extends Cat> covariantCats = cats; // Wilcard - upper bound:
+        Animal animal1 = covariantCats.get(0);
+        Animal animal2 = covariantCats.get(1);
+        Cat cat = covariantCats.get(2);
+        RedCat redCat = (RedCat)covariantCats.get(3);
+        CbbaCat cbbaCat = (CbbaCat)covariantCats.get(4);
+        Animal animal3 = covariantCats.get(5);
 
+        // Wilcard - lower bound:
         List<? super RedCat> contravariantCats = new ArrayList<RedCat>();
         contravariantCats.add(new RedCat());
         contravariantCats.add(new BolivianCat());
         contravariantCats.add(new CbbaCat());
+        // contravariantCats.add(new Cat()); //Fails coz Cat is parent of RedCat
     }
 
     static void addCat(List<? super Cat> cats) {
